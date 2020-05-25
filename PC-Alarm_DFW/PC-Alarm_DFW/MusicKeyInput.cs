7a744d6ced3b6782face8b75ed109e18cf2155ff
noname <48761044+noname0310@users.noreply.github.com>
2020-05-25 18:00:44 +0900

    update

--- PC-Alarm_DFW/PC-Alarm_DFW/MusicKeyInput.cs
+++ PC-Alarm_DFW/PC-Alarm_DFW/MusicKeyInput.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 using System.Threading;
-using System.Windows.Input;
+using System.Drawing;
+using System.Windows.Forms;
 
 namespace PC_Alarm
 {
@@ -18,6 +19,15 @@ namespace PC_Alarm
         private const int SPI_SETSCREENSAVEACTIVE = 0x11;
         private const int SPIF_SENDWININICHANGE = 2;
 
+        private const uint DESKTOP_WRITEOBJECTS = 0x0080;
+        private const uint DESKTOP_READOBJECTS = 0x0001;
+        private const int WM_CLOSE = 16;
+        
+        [DllImport("user32.dll")]
+        static extern bool PostMessage(IntPtr hWnd, uint Msg, int wParam, int lParam);
+
+        const uint WM_KEYDOWN = 0x0100;
+
         [DllImport("user32.dll")]
         public static extern void keybd_event(byte virtualKey, byte scanCode, uint flags, IntPtr extraInfo);
 
@@ -27,11 +37,64 @@ namespace PC_Alarm
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         private static extern bool SystemParametersInfo(int uAction, int uParam, ref bool lpvParam, int flags);
 
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern bool EnumDesktopWindows(
+           IntPtr hDesktop, EnumDesktopWindowsProc callback,
+           IntPtr lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern bool IsWindowVisible(
+           IntPtr hWnd);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern int PostMessage(IntPtr hWnd,
+      int wMsg, int wParam, int lParam);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr OpenDesktop(
+           string hDesktop, int Flags, bool Inherit,
+           uint DesiredAccess);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern bool CloseDesktop(
+           IntPtr hDesktop);
+        // Callbacks
+        private delegate bool EnumDesktopWindowsProc(
+           IntPtr hDesktop, IntPtr lParam);
+
         public static void SetScreenSaverActive(int Active)
         {
             int nullVar = 0;
 
             SystemParametersInfo(SPI_SETSCREENSAVEACTIVE, Active, ref nullVar, SPIF_SENDWININICHANGE);
+        }
+        public static void KillScreenSaver()
+        {
+            IntPtr hDesktop = OpenDesktop("Screen-saver", 0,
+               false, DESKTOP_READOBJECTS | DESKTOP_WRITEOBJECTS);
+            if (hDesktop != IntPtr.Zero)
+            {
+                EnumDesktopWindows(hDesktop, new
+                   EnumDesktopWindowsProc(KillScreenSaverFunc),
+                   IntPtr.Zero);
+                CloseDesktop(hDesktop);
+            }
+            else
+            {
+                PostMessage(GetForegroundWindow(), WM_CLOSE,
+                   0, 0);
+            }
+        }
+
+        private static bool KillScreenSaverFunc(IntPtr hWnd,
+           IntPtr lParam)
+        {
+            if (IsWindowVisible(hWnd))
+                PostMessage(hWnd, WM_CLOSE, 0, 0);
+            return true;
         }
 
         public static void PreviousTrack()
@@ -44,9 +107,8 @@ namespace PC_Alarm
         }
         public static void PauseTrack()
         {
-            System.Windows.Input.
-            Cursor.SetCursor();
-            Thread.Sleep(1000);
+            KillScreenSaver();
+            Thread.Sleep(3000);
             // Play or Pause music
             keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_EXTENTEDKEY, IntPtr.Zero);
             keybd_event(VK_MEDIA_PLAY_PAUSE, 0, KEYEVENTF_KEYUP, IntPtr.Zero);
